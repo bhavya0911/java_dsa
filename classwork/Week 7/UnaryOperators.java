a63e41f61ff461bebfd391b654ef764fbdc8b037
bhavya0911 <bhavya0911@gmail.com>
2024-12-13 17:07:49 +0530

    Week 7

---
+++ classwork/Week 7/UnaryOperators.java
@@ -0,0 +1,9 @@
+public class UnaryOperators {
+    public static void main(String args[]) {
+        int a = 45;
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(a--);
+        System.out.println(--a);
+    } 
+}
