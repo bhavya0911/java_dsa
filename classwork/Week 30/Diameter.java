be79eabc57473379ae439fab9d8eea3ef5f9c865
bhavya0911 <bhavya0911@gmail.com>
2025-02-01 22:06:13 +0530

    Week 30

---
+++ classwork/Week 30/Diameter.java
@@ -0,0 +1,57 @@
+public class Diameter {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static int height(Node root) {
+        if(root == null) {
+            return 0;
+        }
+        int leftCount = height(root.left);
+        int rightCount = height(root.right);
+        return Math.max(leftCount, rightCount) + 1;
+    }
+
+    static class Info {
+        int diameter;
+        int height;
+
+        public Info(int diameter, int height) {
+            this.diameter = diameter;
+            this.height = height;
+        }
+    }
+
+    public static Info diameter(Node root) {
+        if(root == null) {
+            return new Info(0, 0);
+        }
+        Info leftInfo = diameter(root.left);
+        Info rightInfo = diameter(root.right);
+
+        int diameter = Math.max(Math.max(leftInfo.diameter, rightInfo.diameter), leftInfo.height + rightInfo.height + 1);
+        int height = Math.max(leftInfo.height, rightInfo.height) + 1;
+
+        return new Info(diameter, height);
+    }
+
+    public static void main(String[] args) {
+            Node root = new Node(1);
+            root.left = new Node(2);
+            root.right = new Node(3);
+            root.left.left = new Node(4);
+            root.left.right = new Node(5);
+            root.right.left = new Node(6);
+            root.right.right = new Node(7);
+
+            System.out.println(diameter(root).diameter);
+    }
+}
